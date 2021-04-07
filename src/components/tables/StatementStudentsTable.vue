06e6d12f7479a66cc658bf18eed22d71aa82f810
BlaD200 <blad600@gmail.com>
2021-04-07 22:08:36 +0300

    Added statement header

---
+++ src/components/tables/StatementStudentsTable.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "StatementStudentsTable"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/view/Statement.vue
+++ src/view/Statement.vue
@@ -1,19 +1,138 @@
 <template>
     <div>
-        <b-row>
-            <b-col>
-                <h2>№ {{ $route.params.id }}</h2>
+        <b-row class="p-2 justify-content-center">
+            <b-col class="bg-white shadow" sm="12" lg="10">
+                <b-row class="mt-4 mb-3">
+                    <b-col>
+                        <h4 class="text-center">ЗАЛІКОВО-ЕКЗАМЕНАЦІЙНА ВІДОМІСТЬ № {{ $route.params.id }}</h4>
+                    </b-col>
+                </b-row>
+
+                <b-row class="justify-content-center">
+                    <b-col lg="10">
+                        <h6 class="statement-header-common">
+                            Освітній рівень
+                            <span class="statement-header-uncommon"><u>{{ statementHeader.eduLevel }}</u></span>
+                        </h6>
+                        <h6 class="statement-header-common">
+                            <span class="statement-header-uncommon"><u>{{ statementHeader.faculty }}</u></span>
+                            Рік навчання <span class="statement-header-uncommon"><u>{{
+                                statementHeader.course
+                            }}</u></span>
+                            Група <span class="statement-header-uncommon"><b>{{ statementHeader.group }}</b></span>
+                        </h6>
+                        <h6 class="statement-header-common">
+                            Дисципліна <span class="statement-header-uncommon"><u>{{ statementHeader.subjectName }}</u></span>
+                        </h6>
+                        <h6 class="statement-header-common">
+                            Семестр <span class="statement-header-uncommon"><u>{{ statementHeader.semester }}</u></span>
+                            Залікові бали <span class="statement-header-uncommon"><u>{{
+                                statementHeader.creditNumber
+                            }}</u></span>
+                        </h6>
+                        <h6 class="statement-header-common">
+                            Форма контролю: <span class="statement-header-uncommon"><u>{{
+                                statementHeader.controlType
+                            }}</u></span>.
+                            Дата <span class="statement-header-uncommon"><u>{{ statementHeader.examDate }}</u></span>
+                        </h6>
+                    </b-col>
+                </b-row>
+
+                <b-row class="mt-3">
+                    <b-col>
+                        <h6 class=" text-center">
+                            <span class="statement-header-uncommon"><u>
+                                {{ statementHeader.tutorFullName }},
+                                {{ statementHeader.tutorAcademicStatus ? `${statementHeader.tutorAcademicStatus},` : '' }}
+                                {{ statementHeader.tutorPosition }}
+                            </u></span>
+                        </h6>
+                    </b-col>
+                </b-row>
+
+                <b-row class="justify-content-center mt-3 mb-5">
+                    <b-col lg="10">
+                        <student-table :students="students" :loading="studentsLoading"></student-table>
+                    </b-col>
+                </b-row>
             </b-col>
         </b-row>
     </div>
 </template>
 
 <script>
+import StudentTable from "@/components/tables/StudentTable";
+
 export default {
-name: "Statement"
+    name: "Statement",
+    components: {StudentTable},
+    data() {
+        return {
+            statementHeader: {
+                statementNo: '',
+                eduLevel: 'Бакалавр',
+                faculty: 'Факультет інформатики',
+                course: 3,
+                group: 1,
+                subjectName: 'Технології сучасних дата - центрів',
+                semester: '6д',
+                creditNumber: '?',
+                controlType: 'екзамен',
+                examDate: '2021-05-24',
+                tutorFullName: 'Черкасов Дмитро Іванович',
+                tutorPosition: 'старший викладач',
+                tutorAcademicStatus: 'кандидат технічних наук'
+            },
+
+            students: [
+                {
+                    "studentCode": 4,
+                    "studentSurname": "Бойко",
+                    "studentName": "Данило",
+                    "studentPatronymic": "Романович",
+                    "studentRecordBook": "37452"
+                }, {
+                    "studentCode": 3,
+                    "studentSurname": "Євтушенко",
+                    "studentName": "Ігор",
+                    "studentPatronymic": "Олегович",
+                    "studentRecordBook": "83921"
+                }, {
+                    "studentCode": 7,
+                    "studentSurname": "Залізний",
+                    "studentName": "Антон",
+                    "studentPatronymic": "Вячеславович",
+                    "studentRecordBook": "32432"
+                }, {
+                    "studentCode": 8,
+                    "studentSurname": "Іванюк",
+                    "studentName": "Назар",
+                    "studentPatronymic": "Олександрович",
+                    "studentRecordBook": "34123"
+                }, {
+                    "studentCode": 6,
+                    "studentSurname": "Крейдун",
+                    "studentName": "Андрій",
+                    "studentPatronymic": "Миколайович",
+                    "studentRecordBook": "33122"
+                }],
+            studentsLoading: false
+        }
+    }
 }
 </script>
 
-<style scoped>
+<style>
+body {
+    background-color: #f8f9fa !important;
+}
 
+.statement-header-common {
+    font-weight: 400;
+}
+
+.statement-header-uncommon {
+    font-weight: 500;
+}
 </style>
